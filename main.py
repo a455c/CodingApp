8305edd9122b6a288969a1a2f188706f6e18a263
a455c <isaacupcroft@gmail.com>
2022-11-16 20:23:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import sys, time
+
+#cool typewriter animation
+def print(sentence):
+  for c in sentence:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+# easy code transfer dictionary
+dict = {
+    'if' : 'if',
+    'less' : '<',
+    'more' : '>',
+    'equals': '=',
+    'is': '==',
+    'than': ' ',
+    'do' : ':',
+    'for': 'for',
+    'and' : 'and',
+    'or' : 'or',
+    'while': 'while',
+    'not' : 'not',
+    'plus': '+',
+    'minus': '-', 
+    'divided': '/',
+    'times': '*',
+    'by' : ' ',
+    "isn't": '!=',
+}
+
+def switch(w): # acts as a switch statement
+    try:
+        var = dict.get(w) #finds the easycode -> python value and stores it
+        if var: # if var is not 'None' is returns the new python syntax
+            return var
+        else: # if var is 'None' it returns the original word and keeps it as a variable
+            return w
+    except:
+        print('exception')
+
+
+
+easycodelst = [] # creates a list for the easycode sentences
+with open('easycodefile.txt', 'r') as file: # opens text file as variable 'file'
+    for sentence in file: # for sentence in file
+        easycodelst.append(sentence) # add the sentence to the list
+
+pylist = []
+
+for sentence in easycodelst:
+    if not sentence:# if there is no sentence, continue
+        continue
+
+    split = sentence.split() # split the sentences into singular word
+
+    for word in split: # for each word in the wordlist
+        word = switch(word) # use the switch function to get the python syntax
+        print(str(word) + ' ') # print the syntax
+
+        pylist.append(word)
+
+    print('\n') # start a new line for every new sentence
+    pylist.append('_')
+
+
+with open('pythonfile.py', 'w') as f:
+    for word in pylist:
+        if word == '_':
+            f.write('\n')
+            continue
+
+        
+        f.write(word + ' ')
+
---
+++ pythonfile.py
@@ -0,0 +1,16 @@
+cheese = 5 
+if cheese <   6 : 
+    cheese = 10 
+else : 
+    cheese = 45 
+
+while cheese != 45 : 
+    cheese = cheese + 1 
+    cheese = cheese *   3 
+    cheese = cheese /   10 
+
+if cheese != 56 : 
+    cheese = 1 
+
+while cheese == 1 : 
+    32 /   4 
